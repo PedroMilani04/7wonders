06b38e4459a48fe11c2164f6c3937f898b067485
Pedro Henrique Milani Vagula <milanipedrovagula@gmail.com>
2024-01-03 12:22:26 -0300

    new wonders scroll and design

--- src/app/components/wonderscontent/wonderscontent.component.ts
+++ src/app/components/wonderscontent/wonderscontent.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import AOS from 'aos'
 
 @Component({
   selector: 'app-wonderscontent',
@@ -8,5 +9,49 @@ import { Component } from '@angular/core';
   styleUrl: './wonderscontent.component.css'
 })
 export class WonderscontentComponent {
+  ngOnInit() {
+    AOS.init();
+  }
 
+  scrollToNextWonder() {
+    const wonders = document.querySelectorAll('.container');
+    const currentScroll = window.scrollY;
+    let nextWonderTop = Infinity;
+
+    // Find the top position of the next wonder
+    wonders.forEach(wonder => {
+        const rect = wonder.getBoundingClientRect();
+        const wonderTop = rect.top + window.scrollY;
+        if (wonderTop > currentScroll && wonderTop < nextWonderTop) {
+            nextWonderTop = wonderTop;
+        }
+    });
+
+    // Scroll smoothly to the next wonder
+    window.scrollTo({
+        top: nextWonderTop,
+        behavior: 'smooth'
+    });
+}
+
+  scrollToPrevWonder() {
+    const wonders = document.querySelectorAll('.container');
+    const currentScroll = window.scrollY;
+    let prevWonderTop = -Infinity;
+
+    // Find the top position of the previous wonder
+    wonders.forEach(wonder => {
+      const rect = wonder.getBoundingClientRect();
+      const wonderTop = rect.top + window.scrollY;
+      if (wonderTop < currentScroll && wonderTop > prevWonderTop) {
+        prevWonderTop = wonderTop;
+      }
+    });
+
+    // Scroll smoothly to the top position of the previous wonder
+    window.scrollTo({
+      top: prevWonderTop,
+      behavior: 'smooth'
+    });
+  }
 }
